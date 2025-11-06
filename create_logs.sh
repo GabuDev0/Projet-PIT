45fcf46fa63e495a7942cb361dff02e707c18a9d
GabuWaSaikou <contact.saikou.games@gmail.com>
2025-11-07 00:31:46 +0100

    FIX: create_logs.sh command not found

--- create_logs.sh
+++ create_logs.sh
@@ -4,23 +4,6 @@ IFS=$'\n\t'
 
 # ./create_logs.sh
 
-# ** CONST
-
-OUTDIR="./var/log"
-NETLOG="$OUTDIR/network.log"
-
-: > "$NETLOG"
-
-LEURRES=()
-NUM_LEURRES=12
-NBR_ENTRIES=10
-
-TARGET_IP=$(generate_random_ip)
-
-CHARS=( {a..z} {A..Z} {0..9} )
-
-KEY=""
-
 generate_random_ip() {
     printf "%d.%d.%d.%d" $((10 + RANDOM % 240)) $((1 + RANDOM % 253)) $((1 + RANDOM % 253)) $((1 + RANDOM % 253))
 }
@@ -97,6 +80,23 @@ write_ack() {
   
     printf "%s\n" "$msg" >> "$NETLOG"
 }
+
+# ** CONST
+
+OUTDIR="./var/log"
+NETLOG="$OUTDIR/network.log"
+
+: > "$NETLOG"
+
+LEURRES=()
+NUM_LEURRES=12
+NBR_ENTRIES=10
+
+TARGET_IP=$(generate_random_ip)
+
+CHARS=( {a..z} {A..Z} {0..9} )
+
+KEY=""
 
 for ((i=0;i<5;i++)); do
     KEY+="${CHARS[RANDOM % ${#CHARS[@]}]}"
